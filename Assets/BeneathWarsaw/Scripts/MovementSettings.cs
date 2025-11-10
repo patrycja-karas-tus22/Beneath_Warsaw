198ba3608694f965d918e082d4c18681566be591
Pati <K00280813@student.tus.ie>
2025-11-10 13:00:06 +0000

    Continuing localization and adding vignette

--- Assets/BeneathWarsaw/Scripts/MovementSettings.cs
+++ Assets/BeneathWarsaw/Scripts/MovementSettings.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 using UnityEngine.UI;
 using UnityEngine.XR.Interaction.Toolkit.Locomotion.Movement;
 using UnityEngine.XR.Interaction.Toolkit.Locomotion.Turning;
@@ -14,10 +16,33 @@ public class MovementSettings : MonoBehaviour
     public ContinuousTurnProvider smoothTurn;
     public SnapTurnProvider snapTurn;
 
+    [Header("Vignette Settings")]
+    public Transform player;
+    public Volume vignetteVolume;
+    public float maxIntensity;
+    public float currentIntensity = 0f;
+    private Vignette vignette;
+    private Vector3 lastPosition;
+    
     private void Start()
     {
+        lastPosition = player.position;
+
         movementSlider.value = dynamicMoveProvider.moveSpeed;
         movementSlider.onValueChanged.AddListener(UpdateSpeed);
+
+        if (vignetteVolume.profile.TryGet<Vignette>(out var v))
+            vignette = v;
+    }
+    private void Update()
+    {
+        float movementThreshold = 0.001f;
+        if (Vector3.Distance(player.position, lastPosition) > movementThreshold)
+            vignette.intensity.value = maxIntensity;
+        else
+            vignette.intensity.value = 0f;
+
+        lastPosition = player.position;
     }
     private void UpdateSpeed(float newSpeed)
     {
@@ -36,5 +61,10 @@ public class MovementSettings : MonoBehaviour
     private void OnDisable()
     {
         movementSlider.onValueChanged.RemoveListener(UpdateSpeed);
+    }
+
+    void VignetteOn()
+    {
+
     }
 }
--- Assets/BeneathWarsaw/Scripts/SettingsMenu.cs
+++ Assets/BeneathWarsaw/Scripts/SettingsMenu.cs
@@ -2,11 +2,14 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Localization.Settings;
 using TMPro;
+using System.Collections;
 using UnityEngine.InputSystem;
 public class SettingsMenu : MonoBehaviour
 {
     public TMP_Dropdown turnTypeDropdown;
     public TMP_Dropdown movementTypeDropdown;
+    public GameObject languagePanel;
+    public GameObject movementPanel;
     private Canvas canvas;
 
     private void Start()
@@ -20,23 +23,51 @@ public class SettingsMenu : MonoBehaviour
             ToggleMenu();
         }
     }
-   /* public void GeneralButtonPress()
-    {
-        Debug.Log("HIIII");
-    }
-        public void SetLanguage(int index)
-    {
-        LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[index];
-        RefreshDropdowns();
-    }
-    void RefreshDropdowns()
-    {
-        if (turnTypeDropdown != null)
-            turnTypeDropdown.RefreshShownValue();
 
-        if (movementTypeDropdown != null)
-            movementTypeDropdown.RefreshShownValue();
-    } */
+    public void LanguageTab()
+    {
+        languagePanel.SetActive(true);
+        movementPanel.SetActive(false);
+
+
+    }
+    public void MovementTab()
+    {
+        movementPanel.SetActive(true);
+        languagePanel.SetActive(false);
+
+    }
+    public TMP_Dropdown languageDropdown;
+
+    public void OnLanguageChanged()
+    {
+        StartCoroutine(SetLanguage(languageDropdown.value));
+    }
+
+    IEnumerator SetLanguage(int index)
+    {
+        yield return LocalizationSettings.InitializationOperation;
+        LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[index];
+    }
+    /* public void SetLanguage(int index)
+     {
+         LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[index];
+         RefreshDropdowns();
+     }
+     void RefreshDropdowns()
+     {
+         if (turnTypeDropdown != null)
+             turnTypeDropdown.RefreshShownValue();
+
+         if (movementTypeDropdown != null)
+             movementTypeDropdown.RefreshShownValue();
+     }
+     /* public void GeneralButtonPress()
+      {
+          Debug.Log("HIIII");
+      }
+
+      */
 
     void ToggleMenu()
     {
